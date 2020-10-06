ade0a6c1886a44876b23c9dec3170787e5fca43e
Shannon Sagehorn <srsagehorn@gmail.com>
2020-10-06 13:45:23 -0500

    handlebars

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const burger = require("../models/burger.js");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  burger.selectAll((data) => {
+    const hbsObject = { burgers: data };
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", (req, res) =>
+  burger.insertOne(["burger_name"], [req.body.burger_name], (result) =>
+    res.json({ id: result.insertId })
+  )
+);
+
+router.put("/api/burgers/:id", (req, res) => {
+  const idString = `id = ${req.params.id}`;
+  burger.updateOne(idString, (result) => {
+    if (result.changedRows === 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
